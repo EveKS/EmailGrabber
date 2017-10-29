067ab478f9a1a67bb5525c3638c7d6317e5179b6
EveKS <vikkes@yandex.ru>
2017-10-29 17:05:10 +0700

    Добавьте файлы проекта.

---
+++ EmailGrabber/ClientApp/app/hub.connection.ts
@@ -0,0 +1,43 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HubConnection } from '@aspnet/signalr-client';
+
+@Component({
+    selector: 'app-hab-component',
+    templateUrl: './home.component.html'
+})
+
+export class HubComponent implements OnInit {
+    private _hubConnection: HubConnection;
+    public async: any;
+    message = '';
+    messages: string[] = [];
+
+    constructor() {
+    }
+
+    public sendMessage(): void {
+        const data = `Sent: ${this.message}`;
+
+        this._hubConnection.invoke('Send', data);
+        this.messages.push(data);
+    }
+
+    ngOnInit() {
+        this._hubConnection = new HubConnection('/chat');
+
+        this._hubConnection.on('Send', (data: any) => {
+            const received = `Received: ${data}`;
+            this.messages.push(received);
+        });
+
+        this._hubConnection.start()
+            .then(() => {
+                console.log('Hub connection started')
+            })
+            .catch(err => {
+                console.log('Error while establishing connection')
+            });
+    }
+
+}
